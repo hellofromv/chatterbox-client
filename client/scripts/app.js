da24ba8b269518db8036539c26e197c5f5b2a226
vickeetran <ptran.vicki@gmail.com>
2016-05-02 15:32:39 -0700

    Made progress

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,41 @@
-// YOUR CODE HERE:
+var app = {};
 
+app.init = function() {
+
+};
+
+app.send = function(message) {
+  $.ajax({
+    type: 'POST',
+    data: JSON.stringify(message),
+    url: 'https://api.parse.com/1/classes/messages'
+  });
+};
+
+app.fetch = function() {
+  $.ajax({
+    type: 'GET'
+  });
+};
+
+app.clearMessages = function() {
+  $('#chats').empty();
+};
+
+app.addMessage = function(message) {
+  $('#chats').append('<blink class=".username">' + JSON.stringify(message) + '</blink>');
+};
+
+app.addRoom = function(roomName) {
+  var room = $('#roomSelect').append('<option value="' + roomName + '">' + roomName + '</option>');
+};
+
+app.friends = [];
+
+app.addFriend = function(friendName) {
+  app.friends.push(friendName);
+};
+
+$('.username').on('click', function(user) {
+  app.addFriend(user.username);
+});
